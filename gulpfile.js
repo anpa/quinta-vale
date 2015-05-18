2ec5782fa3bba3dccbb748243a9c363cfbafe9d0
Andre Alves <andrenpalves@gmail.com>
2015-05-18 23:45:36 +0200

    Structural changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,39 @@
+// gulp
 var gulp = require('gulp');
+
+// plugins
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
+
+// TASKS
+
+//compile sass
 gulp.task('sass', function () {
-    gulp.src('css/all.scss')
+    gulp.src('app/css/all.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('app/css'))
         .pipe(reload({ stream:true }));
 });
 
-// watch files for changes and reload
-gulp.task('server', ['sass'], function() {
+//start server
+gulp.task('server', function() {
   browserSync({
     server: {
-      baseDir: ''
-    }
+      baseDir: 'app'
+    },
+    port: process.env.PORT || 3000
   });
-
-  gulp.watch('css/*.scss', ['sass']);
-  gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
 });
+
+//run project
+gulp.task('start', ['sass', 'server']);
+
+//update css
+gulp.watch('app/css/*.scss', ['sass']);
+//live reload
+gulp.watch(['*.html', 'app/css/**/*.css', 'app/js/**/*.js'], {cwd: 'app'}, reload);
